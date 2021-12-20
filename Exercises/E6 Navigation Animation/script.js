2f45a9e1b3c6bc83a6f1972b42ff9f37fa606420
April-Yefei-Guo <90733719+April1206@users.noreply.github.com>
2021-12-19 22:03:18 -0500

    Create script.js

---
+++ Exercises/E6 Navigation Animation/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.js-button');
+
+button,addEventListener('click',secretSauce);
+
+function secretSauce(){
+  alert('yefei!')
+  botton.innerHTML = "<p>I am a button</p>"
+}
