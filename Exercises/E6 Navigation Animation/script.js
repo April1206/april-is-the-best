3cdb8cf23b101e7829ad76c81cd43ca67ed8ba35
April-Yefei-Guo <90733719+April1206@users.noreply.github.com>
2021-12-21 00:23:56 -0500

    Update script.js

--- Exercises/E6 Navigation Animation/script.js
+++ Exercises/E6 Navigation Animation/script.js
@@ -3,6 +3,7 @@ const button = document.querySelector('.js-button');
 button,addEventListener('click',secretSauce);
 
 function secretSauce(){
+  
   alert('wanna🥥?')
   botton.innerHTML = "<p>I am a button</p>"
 }
